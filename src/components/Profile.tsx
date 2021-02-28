a3e5227df8851e46cca9d05f87ca23a5705ce194
gpmrs <giovannapmr@gmail.com>
2021-02-28 12:54:23 -0300

    aula 03

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -4,7 +4,7 @@ export function Profile() {
         <div className={styles.profileContainer}>
             <img src="https://github.com/gpmrs.png" alt="gpmrs" />
             <div>
-                <strong>Giovanna Petraglia</strong>
+                <strong>Mina Harker</strong>
                 <p>
                     <img src="icons/level.svg" alt="level" />
                     Level 1
